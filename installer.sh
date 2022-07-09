52cef0e90038c5b2ec390e78a1601931f09367dc
Tyler <xeroxmc6@gmail.com>
2022-07-09 13:50:07 -0500

    Update installer.sh
    Split up into functions to make editing in the future easier

--- installer.sh
+++ installer.sh
@@ -9,9 +9,8 @@
 #MAX FANSPEED SET
 #sudo ectool fanduty 100 && sudo ectool pwmgetfanrpm | grep -o '[0-9]*' | awk '$1>0'
 
-if [ "$1" == "install" ]
-then
-
+buildEctool(){
+    
     set -xe
     echo "Downloading dependencies..."
     git clone https://chromium.googlesource.com/chromiumos/platform/ec --depth 1
@@ -25,22 +24,26 @@ then
     read BOARD
     make BOARD=$BOARD CROSS_COMPILE= HOST_CROSS_COMPILE= build/$BOARD/util/ectool || echo "Compiling error or invalid board supplied."
 
-    #Building ectool
-    cp build/$BOARD/util/ectool /usr/bin/ectool
-    cd ..
+}
 
-    #configure the maximum fan speed
+buildController(){
+
+    g++ -std=c++11 ./controller/EcToolControl.cpp ./controller/configManipulation.hpp ./controller/installManipulation.hpp ./controller/directories.h -o ./EcToolControl
+
+}
+
+getMaxSpeed(){
+
     echo "Setting fan(s) to 100% to get maximum rpm approximation for configuration file"
     sudo ectool fanduty 100 
     sleep 1
     MAX_RPM=`sudo ectool pwmgetfanrpm | grep -o '[0-9]*' | awk '$1>0'`
     sed -i "5s/.*/MAX_FAN_SPEED=$MAX_RPM/" ./scripts/chromebook-fan-control.sh
 
-    #building the configuration utility
-    g++ -std=c++11 ./controller/EcToolControl.cpp ./controller/configManipulation.hpp ./controller/installManipulation.hpp ./controller/directories.h -o ./EcToolControl
-    cp EcToolControl /usr/bin/EcToolControl
+}
 
-    #setting up the services and timers
+setupServices(){
+
     cp ./scripts/chromebook-fan-control.sh /usr/bin/chromebook-fan-control.sh
     chmod +x /usr/bin/chromebook-fan-control.sh
     cp ./services/fan.service /etc/systemd/system/fan.service
@@ -49,6 +52,27 @@ then
 
     systemctl start fan.timer && systemctl enable fan.timer
     systemctl enable root-resume.service
+
+}
+
+if [ "$1" == "install" ]
+then
+
+    buildEctool
+
+    #Building ectool
+    cp build/$BOARD/util/ectool /usr/bin/ectool
+    cd ..
+
+    #configure the maximum fan speed
+    getMaxSpeed
+
+    #building the configuration utility
+    buildController
+    cp EcToolControl /usr/bin/EcToolControl
+
+    #setting up the services and timers
+    setupServices
 
     echo "Ectool has been installed and the control utility along with the fan service has been set up! restart your computer now to start the service."
 
@@ -61,23 +85,12 @@ then
 elif [ "$1" == "build-controller" ]
 then
 
-    g++ -std=c++11 ./controller/EcToolControl.cpp ./controller/configManipulation.hpp ./controller/installManipulation.hpp ./controller/directories.h -o ./EcToolControl
+    buildController
 
 elif [ "$1" == "build-ec" ]
 then
 
-    set -xe
-    echo "Downloading dependencies..."
-    git clone https://chromium.googlesource.com/chromiumos/platform/ec --depth 1
-    #sed -i -e 's/<libusb.h>/<libusb-1.0\/libusb.h>/g' /tmp/file.txt
-    sudo apt update; sudo apt install git libftdi-dev libusb-dev libusb-1.0-0-dev libncurses5-dev pkgconf g++ lm-sensors -y
-
-    echo "Compiling ectool..."
-    cd ec
-    echo "What board do you have? (You can find this out by finding your 'Board Name' in https://mrchromebox.tech/#devices)"
-    echo
-    read BOARD
-    make BOARD=$BOARD CROSS_COMPILE= HOST_CROSS_COMPILE= build/$BOARD/util/ectool || echo "Compiling error or invalid board supplied."
+    buildEctool
 
 else
 
